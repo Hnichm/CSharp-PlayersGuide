8c37822028764c5cbeb0e59e64bedf5f0503959a
Nicholas <125336099+Hnichm@users.noreply.github.com>
2023-10-07 22:21:57 -0500

    Add files via upload

---
+++ Area of Triangle Console Application/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please enter the base length of the triangle.");
+int triangleBase = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the height of the triangle.");
+int triangleHeight = Convert.ToInt32(Console.ReadLine());
+int triangleArea = triangleBase * triangleHeight / 2;
+Console.WriteLine($"Area of triangle: {triangleArea}");
---
+++ Console 2.0 (Defense of Consola)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Title = "The Defense of Consolas";
+
+Console.Write("Target row? ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Target column? ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"({targetRow - 1}, {targetColumn})");
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.Beep(100, 500);
