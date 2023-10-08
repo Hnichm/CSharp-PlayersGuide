4219f381d3a0778060f1199b9b084de8f3861bbb
Nicholas <125336099+Hnichm@users.noreply.github.com>
2023-10-07 22:21:03 -0500

    Add files via upload

---
+++ Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please enter number of chocolate eggs gathered.");
+int numChocolateEggsGathered = int.Parse(Console.ReadLine());
+int numChocolateEggsPerSister = numChocolateEggsGathered / 4;
+int numChocolateEggsForDuckbear = numChocolateEggsGathered % 4;
+Console.WriteLine($"Number of eggs per sister: {numChocolateEggsPerSister}\nNumber of eggs for Duckbear {numChocolateEggsForDuckbear}");
+
