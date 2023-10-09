06160d8cb58b6d51234255ea0cb52ffa501cf97d
Nicholas <125336099+Hnichm@users.noreply.github.com>
2023-10-09 16:48:20 -0500

    Add files via upload

---
+++ Taking A Number/Program.cs
@@ -0,0 +1,30 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int result = AskForNumber("What is the airspeed velocity of an unladen swallow?");
+
+int AskForNumberInRange(string text, int min, int max)
+{
+   
+    while (true)
+    {
+        Console.WriteLine(text);
+        int answer = Convert.ToInt32(Console.ReadLine());
+        if (answer > min && answer < max)
+        {
+         
+            return answer;
+        }
+        else
+        {
+            Console.WriteLine($"Please write a number between {min} and {max}");
+        }
+    }
+    
+}
+
+AskForNumberInRange("Boop", 10, 20);
