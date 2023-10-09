1ef36b6b1c6285f3d2eae010e4555f8f61c12491
Nicholas <125336099+Hnichm@users.noreply.github.com>
2023-10-09 15:04:57 -0500

    Add files via upload

---
+++ The Laws of Freach/Program.cs
@@ -0,0 +1,18 @@
+﻿// Original Freach array for loop
+//int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+//int total = 0;
+//for (int index = 0; index < array.Length; index++)
+//    total += array[index];
+//float average = (float)total / array.Length;
+//Console.WriteLine(average);
+
+
+// New Freach foreach array loop
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+foreach (int num in array)
+    total += num;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
+
+
